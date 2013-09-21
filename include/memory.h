944bd7d99dda7185a4d455df2f5951fdcfeaa3c3
Levente Kurusa <levex@linux.com>
2013-09-21 13:09:04 +0200

    Fixed memutils. (whoops)

---
+++ include/memory.h
@@ -0,0 +1,9 @@
+#ifndef __MEMORY_H_
+#define __MEMORY_H_
+
+#include <stdint.h>
+#include <stddef.h>
+extern void* memcpy(const void* dest, const void* src, size_t num );
+extern void* memset (void * ptr, int value, size_t num );
+
+#endif
---
+++ memory/memutils.c
@@ -0,0 +1,35 @@
+/** @author Levente Kurusa <levex@linux.com> **/
+#include "../include/memory.h"
+#include <stdint.h>
+#include <stddef.h>
+
+
+void* memcpy(const void* dest, const void* src, size_t count )
+{
+	char* dst8 = (char*)dest;
+	char* src8 = (char*)src;
+
+	if (count & 1) {
+		dst8[0] = src8[0];
+		dst8 += 1;
+		src8 += 1;
+	}
+
+	count /= 2;
+	while (count--) {
+		dst8[0] = src8[0];
+		dst8[1] = src8[1];
+
+		dst8 += 2;
+		src8 += 2;
+	}
+	return dest;
+}
+
+void* memset (void * ptr, int value, size_t num )
+{
+	unsigned char* p=ptr;
+	while(num--)
+		*p++ = (unsigned char)value;
+	return ptr;
+}
